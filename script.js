d2cc864e7773e651e251c11d95bf4717a70a2b9f
tariellaelius <tariellaelius@users.noreply.github.com>
2017-03-23 00:18:27 +0400

    Added some more styles, modified permissions and added script to fetch and display weather data

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 $(function() {
 
-var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+var $weather = $('.weather');    
+    
+if(navigator.geolocation) { 
+  navigator.geolocation.getCurrentPosition(function(position) { 
+    var latitude = Math.round(position.coords.latitude);
+    var longitude = Math.round(position.coords.longitude);
+    var url = 'https://api.apixu.com/v1/current.json?key=981a4177d5834ae89f9180839172203&q=' + latitude + ',' + longitude;
+    
 
-var bgStyle = 'url("https://source.unsplash.com/category/nature/' + width + 'x' + height + '")';
+  $.getJSON(url, function(json) {
+      
+      var temp = json.current.temp_c + '\u00B0 C';
+      var condition = json.current.condition.text;
+      
+      $weather.append($('<p>').text(temp));
+      $weather.append($('<p>').text(condition));
 
-$('body').css('background', bgStyle);
+      
+  });
+  });
+}
 
 });
