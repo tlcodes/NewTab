b5e2d45d7830e1289eab683cfde7491770d48152
tariellaelius <tariellaelius@users.noreply.github.com>
2017-03-28 11:32:13 +0400

    Daily focus feature is completed

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(function() {
 
 var $weather = $('.weather');    
-var goalInput = $('.mainForm input');
+var $goalInput = $('.mainForm input');
 var $clock = $('.clock');
 var $datep = $('.date');
+
 
 
 if(navigator.geolocation) { 
@@ -28,7 +29,7 @@ if(navigator.geolocation) {
       
       var $icon = $("<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>")
       $weather.append($icon);
-      $weather.append($('<p>').html(cels + '\u00B0 C'));
+      $weather.append($('<p>').text(cels + '\u00B0 C'));
 //      $weather.append($('<p>').text(condition));
       $weather.append($('<p>').text(location));
 
@@ -40,15 +41,48 @@ if(navigator.geolocation) {
 
 
 function saveMainGoal(event) {
-    event.preventDefault();
-    var goal = goalInput.val();
-    if(goal) {
-       chrome.storage.sync.set({'mainGoal': goal});
-    }    
+    
 }
     // $('.mainForm input').eq(0).focus();   // Chrome forbids the extensions that substitute the new tab page from grabbing focus from the omnibar
 
-$('.mainForm').on('submit', saveMainGoal);
+// Hide one element, display another one and do it smoothly    
+    
+function transitionSmoothly(toHide, toDisplay) {
+    toHide.css('opacity', 0);       // used in conjunction with the css 'transition' property to make it gradually disappear
+    setTimeout(function() {
+           toHide.css('display', 'none');   // when transition is over (rough approximation of 1000ms), remove the element from the document flow
+           toDisplay.css('display', 'block');   // and add the other one instead - default CSS value for opacity is set to 0, so it won't appear yet
+           setTimeout(function() {
+               toDisplay.css('opacity', '1');   // A slight delay is needed before setting the opacity to 1, otherwise 'transition' effect will be ignored
+           }, 10);                              // because the 'display' property for this element has just been modified
+       }, 1000);
+}
+    
+    
+// attach 'submit' event handler to daily focus prompt    
+    
+$('.mainForm').on('submit', function(event) {
+    event.preventDefault();     // prevent the page from reloading, which is the default action for 'submit' event
+    var goal = $goalInput.val();
+    if(goal) {                  // ensure that the input is not empty
+       chrome.storage.sync.set({'mainGoal': goal});     // store the input text
+       $('#submittedGoal + label > span').text(goal);   
+       transitionSmoothly($('.goalPrompt'), $('.submittedGoalContainer'));
+    }
+    
+});
+
+// attach event handler to the 'x' button to remove the daily focus
+
+$('#removeDailyGoal').on('click', function() {    
+    $goalInput.val('');    
+    chrome.storage.sync.set({'mainGoal': ''});    // remove the daily focus text from the storage
+    transitionSmoothly($('.submittedGoalContainer'), $('.goalPrompt'));    
+    setTimeout(function() {                         // set the checkbox contents to nothing and reset its 'checked' property after enough time has passed, allowing the 
+        $('#submittedGoal + label > span').text('');// containing div to disappear
+        $('#submittedGoal').prop('checked', false);
+    }, 1000);
+});
     
     // Test if the submit text has been saved
       chrome.storage.onChanged.addListener(function(changes, namespace) {
@@ -62,41 +96,55 @@ $('.mainForm').on('submit', saveMainGoal);
                       storageChange.newValue);
         }
       });
-
-var myVar = setInterval(function() {
-  theTime();
-}, 1000);    
-function theTime() {
       
+var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+var refreshInterval = setInterval(theTime, 1000);  
+
+function padZero(n) {
+  if(n < 10)
+    return '0' + n;
+  else
+    return n;
+}
+
+
+
+function theTime() {
+    
     var now = new Date();
-    var date = now.getDate();
-    var day = now.getDay();
-    var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    var date = padZero(now.getDate());
+
+    var day = now.getDay();    
+
     var today = dayNames[day];
-    var month = now.getMonth() + 1;    
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    
 
-    if (month < 10)
-        {
-            month = '0' + month;
-        }
-    if (date < 10)
-        {
-            date = '0' + date;
-        }
-    if (minutes < 10)
-        {
-            minutes = '0' + minutes;
-        }
-    
-    $datep.html(today + ', ' + month + '/' + date);
-    $datep.addClass('date');
+    var month = padZero(now.getMonth() + 1);    
+
+    var hours = padZero(now.getHours());
+
+    var minutes = padZero(now.getMinutes());
+  
+
+    $datep.html(today + ', ' + month + '/' + date);    
+
     $clock.html(hours + ':' + minutes);
-    $clock.addClass('clock');
-
-    }
-   
+    
+}  
+    
+    // on page load, check if there is previous text stored
+    
+    chrome.storage.sync.get('mainGoal', function(goal) {
+        if(goal.mainGoal) {                         // if there is already a stored text, display it
+            $('.submittedGoalContainer').css({'display' : 'block', 'opacity' : 1});
+            $('#submittedGoal + label > span').text(goal.mainGoal);            
+        }
+        else {      // otherwise, display the prompt
+            $('.goalPrompt').css({'display' : 'block', 'opacity' : 1});
+        }
+    });
+    
+    
 
 });
